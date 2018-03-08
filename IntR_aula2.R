0317fcb63f1c916ad3a217ddeb0d6894b1684d6a
Estatística é com R! <32965816+EstatisticaComR@users.noreply.github.com>
2018-03-08 19:35:52 -0300

    Add files via upload

---
+++ IntR_aula2.R
@@ -0,0 +1,45 @@
+##Aula2 - Operações Básicas de Matemática
+
+5+4    #adição
+
+6-2    #subtração
+
+7*3    #multiplicação
+
+81/9   #divisão
+
+2^2    #potência
+
+sqrt(121) #raiz
+
+exp(0)  #exponencial
+
+log(1)  #log na base e
+
+log10(1) #log na base 10
+
+##Exercícios 
+
+#1.Calcule 4 elevado ao cubo, multiplicado por 5
+
+
+#2.Calcule o logaritmo na base 10 de 1000
+
+
+#3.Calcule o exponencial de 2.5, elevado a potência -1.3
+
+
+
+##Respostas:
+
+#1. 
+4^3*5
+
+#2.
+log10(1000)
+
+
+#3.
+exp(2.5)^-1.3
+
+#Parabéns, você completou a lição 2. Vá para aula 3 do curso.
